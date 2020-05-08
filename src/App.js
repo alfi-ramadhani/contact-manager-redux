ad9d8805a8e36adc12084a54c8b89856f3c20a4d
Alfi Ramadhani <info.alfistudio@gmail.com>
2020-05-09 01:05:39 +0800

    Add redux

--- src/App.js
+++ src/App.js
@@ -8,27 +8,32 @@ import Footer from './components/layout/Footer';
 import About from './components/pages/About';
 import NotFound from './components/pages/NotFound';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header branding="Contact Manager" />
-          <div className="container" id="mainbody">
-            <Switch>
-              <Route exact path="/" component={Contacts} />
-              <Route exact path="/contact/add" component={AddContact} />
-              <Route exact path="/contact/edit/:id" component={EditContact} />
-              <Route exact path="/about" component={About} />
-              <Route component={NotFound} />
-            </Switch>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container" id="mainbody">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contact/add" component={AddContact} />
+                <Route exact path="/contact/edit/:id" component={EditContact} />
+                <Route exact path="/about" component={About} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>      
     );
   }
 }
